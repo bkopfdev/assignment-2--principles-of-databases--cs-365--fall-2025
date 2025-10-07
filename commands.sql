6dbf4055871076253ee1eab3199d9261e6f31768
bkopfdev <bkopf8@gmail.com>
2025-10-07 13:38:10 -0400

    Added commands

--- commands.sql
+++ commands.sql
@@ -1,19 +1,75 @@
 -- Command 1: Create a new entry into the database, which already has your ten initial entries.
 
+/*First command will create a new user for code-warrior (hey that's you!).
+  Second will add GitHub as a new website.
+  Third will add the relation between the two, and password.*/
+INSERT INTO users (username, fname, lname, email)
+VALUES
+  ("code-warrior", "Roy", "Vanegas", "codewarrior@github.org");
+
+INSERT INTO websites (webName, webUrl)
+VALUES
+  ("GitHub", "github.com");
+
+INSERT INTO accounts (userId, webId, password, comment)
+VALUES
+  (7, 8, AES_ENCRYPT("roysPass1234", @key_str, @init_vector), "The safest password ever!");
+
 
 -- Command 2: Get the password associated with the URL of one of your ten entries.
+
+/*I am casting the decrypted password as a CHAR to make it readable in the output
+  I'm using LIMIT 1 to only get one entry, as the command specifies, but you may remove that line to see all results for that url*/
+SELECT CAST(AES_DECRYPT(password, @key_str, @init_vector) AS CHAR) AS "First Pass for https://facebook.com/"
+  FROM accounts
+  JOIN websites USING (webId)
+  WHERE webUrl = "https://facebook.com/"
+  LIMIT 1;
 
 
 -- Command 3: Get all the password-related data, including the *decrypted* password, associated with URLs that have `https` in two of your ten entries.
 
+/*I am casting the decrypted password again, like in command 2
+  Getting the username from users table for more context in the passwords, and referencing the webUrl from websites table
+  I added LIMIT 2 because this command specifies "in two of your ten entries". However, you may remove that line to see all results*/
+SELECT CAST(AES_DECRYPT(password, @key_str, @init_vector) AS CHAR) AS "2 Passwords for https URLS",
+  users.username,
+  accounts.userId,
+  accounts.webId,
+  accounts.comment,
+  accounts.timeStamp
+  FROM accounts
+  JOIN websites USING (webId)
+  JOIN users USING (userId)
+  WHERE webUrl LIKE "https%"
+  LIMIT 2;
+
 
 -- Command 4: Change a URL associated with one of the passwords in your ten entries.
+
+-- Although a little backwards, this script changes the URL and website name from X to Twitter.
+UPDATE websites SET webUrl = "https://twitter.com/" WHERE webUrl = "https://x.com/";
+UPDATE websites SET webName = "Twitter" WHERE webName = "X";
 
 
 -- Command 5: Change the password to any entry.
 
+-- This will change the first user's (userId 1) password for Youtube (webId 1)
+UPDATE accounts
+  SET password = AES_ENCRYPT("newPassword!", @key_str, @init_vector)
+  WHERE userId = 1
+  AND webId = 1;
+
 
 -- Command 6: Remove a tuple based on a URL.
 
+-- Removes all tuples (account entries) that are for Club Penguin, because Club Penguin doesn't exist anymore :(
+DELETE FROM accounts
+  WHERE webId = (SELECT webId FROM websites WHERE webUrl = "https://clubpenguin.com/");
+
 
 -- Command 7: Remove a tuple based on a password.
+
+-- Will remove the tuple entry for code-warrior that was made in command 1 (sorry!)
+DELETE FROM accounts
+  WHERE password = AES_ENCRYPT("roysPass1234", @key_str, @init_vector);
--- setup.sql
+++ setup.sql
@@ -6,57 +6,65 @@ SET block_encryption_mode = 'aes-256-cbc';
 SET @key_str = UNHEX(SHA2('my secret passphrase', 512));
 SET @init_vector = RANDOM_BYTES(16);
 
+-- users table stores the user information, and uses its id as its primary key
 CREATE TABLE IF NOT EXISTS users (
-  userId      SMALLINT(5)     NOT NULL,
+  userId      SMALLINT(5)     NOT NULL AUTO_INCREMENT,
   username    VARCHAR(128)    NOT NULL,
   fname       VARCHAR(128)    NOT NULL,
   lname       VARCHAR(128)    NOT NULL,
   email       VARCHAR(128)    NOT NULL,
 
-    PRIMARY KEY (user_id)
+  PRIMARY KEY (userId)
 );
 
+-- websites table stores website name and URL, using its id as its primary key
 CREATE TABLE IF NOT EXISTS websites (
-  webId       SMALLINT(5)     NOT NULL,
+  webId       SMALLINT(5)     NOT NULL AUTO_INCREMENT,
   webName     VARCHAR(128)    NOT NULL,
   webUrl      VARCHAR(255)    NOT NULL,
 
-    PRIMARY KEY (webs_id)
+  PRIMARY KEY (webId)
 );
 
-CREATE TABLE IF NOT EXISTS passwords (
-  passId      SMALLINT(5)     NOT NULL,
-  password    VARBINARY(512)  NOT NULL,
-  comment     TEXT NOT NULL,
-  time_stamp DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-
-    PRIMARY KEY (pass_id)
-);
-
-CREATE TABLE IF NOT EXISTS log_passwords (
-  passId      SMALLINT(5)     NOT NULL,
-  webId       SMALLINT(5)     NOT NULL,
+-- accounts table is to have relation between the users and websites, and add the encrypted password and comment
+CREATE TABLE IF NOT EXISTS accounts (
   userId      SMALLINT(5)     NOT NULL,
+  webId       SMALLINT(5)     NOT NULL,
+  password    VARBINARY(512)  NOT NULL,
+  comment     VARCHAR(512),
+  timeStamp DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 
-    PRIMARY KEY (pass_id, webs_id, user_id)
+  PRIMARY KEY (userId, webId) -- Same user cant have the same user information for the same website
 );
 
-INSERT INTO users
+INSERT INTO users (username, fname, lname, email)
 VALUES
-  (00001, "userone", "John", "Yousir", "johnsir@user.org"),
-  (00002, "usertwo", "Michael", "Smith", "mike@user.org");
+  ("userone", "John", "Yousir", "johnsir@user.org"),
+  ("usertwo", "Michael", "Smith", "mike@user.org"),
+  ("userthree", "Ally", "Jones", "ajones@gmail.com"),
+  ("userfour", "Mary", "Smith", "marymary@yahoo.org"),
+  ("userfive", "John", "Michaels", "jman2@gmail.com"),
+  ("usersix", "Natalie", "Night", "nn123@user.org");
 
-INSERT INTO websites
+INSERT INTO websites (webName, webUrl)
 VALUES
-  (00001, "Youtube", "https://youtube.com/"),
-  (00002, "Club Penguin", "https://clubpenguin.com/");
+  ("Youtube", "https://youtube.com/"),
+  ("Club Penguin", "https://clubpenguin.com/"),
+  ("Facebook", "https://facebook.com/"),
+  ("X", "https://x.com/"),
+  ("Reddit", "https://reddit.com/"),
+  ("Instagram", "https://instagram.com/"),
+  ("LinkedIn", "https://linkedin.com/");
 
-INSERT INTO passwords
+INSERT INTO accounts (userId, webId, password, comment)
 VALUES
-(00001, AES_ENCRYPT("pass1234", @key_str, @init_vector), "don't forget this password!!"),
-(00002, AES_ENCRYPT("word5678", @key_str, @init_vector), "");
-
-INSERT INTO log_passwords
-VALUES
-(00001, 00001, 00001),
-(00002, 00002, 00002);
+(1, 1, AES_ENCRYPT("pass1234", @key_str, @init_vector), "don't forget this password!!"),
+(1, 3, AES_ENCRYPT("johnuserspass1234", @key_str, @init_vector), "My facebook one"),
+(2, 2, AES_ENCRYPT("word5678", @key_str, @init_vector), NULL),
+(2, 6, AES_ENCRYPT("mikeinsta!23", @key_str, @init_vector), "I upload pics here!"),
+(3, 3, AES_ENCRYPT("RonDog1111", @key_str, @init_vector), "Name of my pet!"),
+(3, 7, AES_ENCRYPT("allyallyli222", @key_str, @init_vector), NULL),
+(4, 2, AES_ENCRYPT("maryrocks!", @key_str, @init_vector), "I miss club penguin.."),
+(4, 3, AES_ENCRYPT("maryfb!123",  @key_str, @init_vector), NULL),
+(5, 4, AES_ENCRYPT("johnybgoode#$%", @key_str, @init_vector), NULL),
+(6, 5, AES_ENCRYPT("natalie2003", @key_str, @init_vector), ".. Maybe I update this one.");
